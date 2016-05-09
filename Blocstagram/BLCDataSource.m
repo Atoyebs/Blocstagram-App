2d791df205cce079771d7c8c39165a06b646de86
Inioluwa Atoyebi <ini@waffleapp.co>
2016-05-09 20:57:07 +0100

    instagram images are finished

--- Blocstagram/BLCDataSource.m
+++ Blocstagram/BLCDataSource.m
@@ -26,6 +26,8 @@
 
 @property (nonatomic, strong) NSString *accessToken;
 
+@property (nonatomic, assign) BOOL thereAreNoMoreOlderMessages;
+
 @end
 
 
@@ -62,14 +64,15 @@
         
         self.accessToken = note.object;
         
-        [self populateDataWithParameters:nil];
+        [self populateDataWithParameters:nil completionHandler:nil];
     }];
 
 }
 
 
 
-- (void) populateDataWithParameters:(NSDictionary *)parameters {
+- (void) populateDataWithParameters:(NSDictionary *)parameters completionHandler:(BLCNewItemCompletionBlock)completionHandler {
+    
     if (self.accessToken) {
         // only try to get the data if there's an access token
         
@@ -92,13 +95,28 @@
                 NSError *webError;
                 NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&webError];
                 
-                NSError *jsonError;
-                NSDictionary *feedDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonError];
-                
-                if (feedDictionary) {
+                if (responseData) {
+                    NSError *jsonError;
+                    NSDictionary *feedDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonError];
+                    
+                    if (feedDictionary) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            // done networking, go back on the main thread
+                            [self parseDataFromFeedDictionary:feedDictionary fromRequestWithParameters:parameters];
+                            if (completionHandler) {
+                                completionHandler(nil);
+                            }
+                        });
+                    } else if (completionHandler) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            completionHandler(jsonError);
+                        });
+                    }
+                }
+                else if (completionHandler) {
                     dispatch_async(dispatch_get_main_queue(), ^{
                         // done networking, go back on the main thread
-                        [self parseDataFromFeedDictionary:feedDictionary fromRequestWithParameters:parameters];
+                        completionHandler(webError);
                     });
                 }
             }
@@ -154,9 +172,31 @@
         }
     }
     
-    [self willChangeValueForKey:@"mediaItems"];
-    _mediaItems = tmpMediaItems;
-    [self didChangeValueForKey:@"mediaItems"];
+    NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+    
+    if (parameters[@"min_id"]) {
+        // This was a pull-to-refresh request
+        
+        NSRange rangeOfIndexes = NSMakeRange(0, tmpMediaItems.count);
+        NSIndexSet *indexSetOfNewObjects = [NSIndexSet indexSetWithIndexesInRange:rangeOfIndexes];
+        
+        [mutableArrayWithKVO insertObjects:tmpMediaItems atIndexes:indexSetOfNewObjects];
+    }
+    else if (parameters[@"max_id"]) {
+        // This was an infinite scroll request
+        
+        if (tmpMediaItems.count == 0) {
+            // disable infinite scroll, since there are no more older messages
+            self.thereAreNoMoreOlderMessages = YES;
+        }
+        
+        [mutableArrayWithKVO addObjectsFromArray:tmpMediaItems];
+    }
+    else {
+        [self willChangeValueForKey:@"mediaItems"];
+        _mediaItems = tmpMediaItems;
+        [self didChangeValueForKey:@"mediaItems"];
+    }
     
 }
 
@@ -217,17 +257,23 @@
 
 -(void)requestNewItemsWithCompletionHandler:(BLCNewItemCompletionBlock)completionHandler {
     
+    self.thereAreNoMoreOlderMessages = NO;
+    
     if (self.isRefreshing == NO) {
         
         self.isRefreshing = YES;
        
         //Need to add images here
+        NSString *minID = [[self.mediaItems firstObject] idNumber];
+        NSDictionary *parameters = @{@"min_id": minID};
         
-        self.isRefreshing = NO;
-        
-        if (completionHandler) {
-            completionHandler(nil);
-        }
+        [self populateDataWithParameters:parameters completionHandler:^(NSError *error) {
+            self.isRefreshing = NO;
+            
+            if (completionHandler) {
+                completionHandler(error);
+            }
+        }];
         
     }
     
@@ -235,17 +281,21 @@
 
 -(void)requestOldItemsWithCompletionHandler:(BLCNewItemCompletionBlock)completionHandler {
     
-    if (self.isLoadingOlderItems == NO) {
+    if (self.isLoadingOlderItems == NO && self.thereAreNoMoreOlderMessages == NO) {
         
         self.isLoadingOlderItems = YES;
         
         //Need to add images here
+        NSString *maxID = [[self.mediaItems lastObject] idNumber];
+        NSDictionary *parameters = @{@"max_id": maxID};
         
-        self.isLoadingOlderItems = NO;
-        
-        if (completionHandler) {
-            completionHandler(nil);
-        }
+        [self populateDataWithParameters:parameters completionHandler:^(NSError *error) {
+            self.isLoadingOlderItems = NO;
+            
+            if (completionHandler) {
+                completionHandler(error);
+            }
+        }];
     }
 }
 
